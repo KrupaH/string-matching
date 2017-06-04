1c67d43d1fb7aa0f3158ab964421d2c6f62dc5b0
KrupaHebbar <krupa.hebbar@gmail.com>
2017-06-04 16:42:50 +0530

    Changed to Snowball stemmer, added output of files sorted by keyword frequency

--- source/find-string.py
+++ source/find-string.py
@@ -3,12 +3,14 @@ import os
 from nltk.stem import *
 from nltk.tokenize import RegexpTokenizer
 import unicodedata
+import operator
 
 #TODO: Mandatory arguments
 parser = argparse.ArgumentParser(description='Keyword search')
 parser.add_argument("-s","--source",help="source file/directory to search for keyword(s)")
 parser.add_argument("-k","--keyword",help="keyword to be found in the file(s)")
 args = parser.parse_args()
+filedict = {}
 
 #Function matching stemmed keyword to stemmed words in the file and storing number of occurrences
 def findWordOccurrences(words,keyword):
@@ -19,32 +21,29 @@ def findWordOccurrences(words,keyword):
     return numOccurrences
 
 #Function to search for the keyword for each line of a source file
-def keywordSearch(source, keyword, outputfile):
-    stemmer = LancasterStemmer()
+def keywordSearch(source, keyword, filename):
+    stemmer = SnowballStemmer("english")
     tokenizer = RegexpTokenizer(r'\w+')
     numOccurrences = 0
-    with open(outputfile,"w") as opfile:
-        while True:
-            line = source.readline()
-            if not line:
-                break
-            #get words from line
-            wordlist = tokenizer.tokenize(line)
-            #stem words
-            wordlist = [stemmer.stem(word) for word in wordlist]
+    while True:
+        line = source.readline()
+        if not line:
+            break
+        #get words from line
+        wordlist = tokenizer.tokenize(line)
+        #stem words
+        wordlist = [stemmer.stem(word) for word in wordlist]
 
-            #normalize unicode strings
-            words = []
-            for word in wordlist:
-                if isinstance(word, unicode):
-                    words = words + unicodedata.normalize('NFKD', word).encode('ascii','ignore')
-                else:
-                    words = words + [word]
+        #normalize unicode strings
+        words = []
+        for word in wordlist:
+            if isinstance(word, unicode):
+                words = words + [unicodedata.normalize('NFKD', word).encode('ascii','ignore')]
+            else:
+                words = words + [word]
 
-            numOccurrences += findWordOccurrences(words,keyword)
-        opfile.write(str(numOccurrences))
-
-        opfile.close()
+        numOccurrences += findWordOccurrences(words,keyword)
+        filedict[filename] = numOccurrences
 
 #TODO: Check if path is valid
 
@@ -58,5 +57,14 @@ else:
     for root, dirs, filenames in os.walk(args.source):
         for f in filenames:
             log = open(os.path.join(root,f), 'r')
-            opfile = "../output/op_"+args.keyword+"_"+f
-            keywordSearch(log,args.keyword,opfile)
+            keywordSearch(log,args.keyword,f)
+
+#sort list of files based on occurrences
+sortedList = sorted(filedict.items(), key=operator.itemgetter(1), reverse=True)
+opfilename = "../output/op_"+args.keyword+".txt"
+opfile = open(opfilename,"w")
+
+#Store ordered ranking of files based on number of occurrences of keyword
+opfile.write("Occurrences\tFiles\n")
+for item in sortedList:
+    opfile.write(str(item[1]) + '\t' + item[0] + '\n')
