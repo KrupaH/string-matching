ba144f4ae61a89b8d27c8353f5078328556b52d5
KrupaHebbar <krupa.hebbar@gmail.com>
2017-06-08 09:37:01 +0530

    Added Knuth-Morris-Pratt keyword search

--- source/find-string.py
+++ source/find-string.py
@@ -18,7 +18,7 @@ filedict = {}
 time_wordmatch = 0
 time_KMP = 0
 
-#Function matching stemmed keyword to stemmed words in the file and storing number of occurrences
+#Function matching stemmed keyword to stemmed words in the file and returning number of occurrences
 def findWordOccurrences(words,keyword):
     numOccurrences = 0
     for word in words:
@@ -26,14 +26,70 @@ def findWordOccurrences(words,keyword):
             numOccurrences += 1
     return numOccurrences
 
+#Helper function to compute the shift table for KMP string matching
+def computeShiftTable(keyword):
+    pos = 1
+    nextCandidate = 0
+    shiftTable = [None for x in range(len(keyword)+1)]
+
+    shiftTable[0] = -1
+
+    while (pos < len(keyword)):
+        if keyword[pos] == keyword[nextCandidate]:
+            shiftTable[pos] = shiftTable[nextCandidate]
+            pos += 1
+            nextCandidate += 1
+        else:
+            shiftTable[pos] = nextCandidate
+            nextCandidate = shiftTable[nextCandidate]
+
+            while (nextCandidate >= 0 and keyword[pos] != keyword[nextCandidate]):
+                nextCandidate = shiftTable[nextCandidate]
+
+            pos += 1
+            nextCandidate += 1
+
+        shiftTable[pos] = nextCandidate
+
+    return shiftTable
+
+
+#Function using enhanced KMP string matching to find number of matches in stemmed word list
+def findKMPMatches(words,keyword, shiftTable):
+    numMatches = 0
+
+    for word in words:
+        #TODO: Convert KMP to enhanced KMP
+        curMatch = 0
+        curChar = 0
+
+        while ((curMatch + curChar) < len(word)):
+            if keyword[curChar] == word[curMatch+curChar]:
+                curChar += 1
+                if curChar == len(keyword):
+                    numMatches += 1
+                    break
+            else:
+                if shiftTable[curChar] > -1:
+                    curMatch += curChar - shiftTable[curChar]
+                    curChar = shiftTable[curChar]
+                else:
+                    curMatch += curChar + 1
+                    curChar = 0
+
+    return numMatches
+
 #Function to search for the keyword for each line of a source file
 def keywordSearch(source, keyword, filename):
     global time_wordmatch, time_KMP
-    numIterations = 0
 
     stemmer = SnowballStemmer("english")
     tokenizer = RegexpTokenizer(r'\w+')
     numOccurrences = 0
+    numMatches = 0
+
+    shiftTable = computeShiftTable(keyword)
+
     while True:
         line = source.readline()
         if not line:
@@ -56,9 +112,14 @@ def keywordSearch(source, keyword, filename):
         numOccurrences += findWordOccurrences(words,keyword)
         end = timer()
         time_wordmatch += (end-start)
-        numIterations += 1
 
         filedict[filename] = numOccurrences
+
+        #Time finding number of word matches using KMP string matching
+        start = timer()
+        numMatches += findKMPMatches(words,keyword,shiftTable)
+        end = timer()
+        time_KMP += (end-start)
 
 #TODO: Check if path is valid
 
@@ -73,7 +134,8 @@ else:
         for f in filenames:
             log = open(os.path.join(root,f), 'r')
             keywordSearch(log,args.keyword,f)
-print("Direct word-comparison search: " + str(time_wordmatch) + " s")
+print("Direct word-comparison search time: " + str(time_wordmatch) + " s")
+print("KMP string-matching search time: "+str(time_KMP)+" s")
 
 
 #sort list of files based on occurrences
